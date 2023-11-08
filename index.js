10f76892826ff9b84ee3a0990a4aee900bd7d35b
himzei <himzei@gmail.com>
2023-11-08 13:41:02 +0900

    1108 13:40

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ console.log(typeof a)
 console.log("나의 이름은"+whoAmI+"이    고, 나의 취미는 " + hobby + "입니다.")
 // console.log(`나의 이름은 ${whoAmI} 이고, 나의 취미는 ${hobby} 입니다.`)
 
+// 숫자배열
 const arr = [1, 2, 3, 4]
-console.log(arr)
+const arr2 = ["조현일", "은성", "정우"]
+const arr3 = ["조현일", 1, 2, "은성"]
+console.log(arr3[2])
+
+// 객체
+const obj1 = {name: "조현일", hobby: "달리기"}
+const obj2 = {name: "조은성", hobby: "유튜브시청"}
+
+const arr4 = [
+    {name: "조현일", hobby: "달리기"},
+    {name: "조은성", hobby: "유튜브시청"}
+]
+
+
+
+
 
