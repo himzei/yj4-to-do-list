d0d8e86c9ef4ac41cb5e880172f68b3afa4aa27e
himzei <himzei@gmail.com>
2023-11-08 12:42:02 +0900

    init

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello js")
+// 변수
+const whoAmI = "조현일"
+let hobby = "달리기"
+
+const a = "10"
+const b = 20
+
+console.log(typeof whoAmI)
+console.log(typeof a)
+
+// " "
+// ' '
+// ` `
+
+
+console.log("나의 이름은"+whoAmI+"이    고, 나의 취미는 " + hobby + "입니다.")
+// console.log(`나의 이름은 ${whoAmI} 이고, 나의 취미는 ${hobby} 입니다.`)
+
+const arr = [1, 2, 3]
+console.log(arr)
+
